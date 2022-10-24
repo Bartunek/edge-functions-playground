071462b4d20d64185a3c0ef9f1bcfc67ed338607
Jan Bartůněk <gsus0187@gmail.com>
2022-10-24 15:23:58 +0200

    Change cookie name to `nf_ab`

--- netlify/edge-functions/cookies.ts
+++ netlify/edge-functions/cookies.ts
@@ -1,26 +1,28 @@
 import type { Context } from "https://edge.netlify.com";
 
+const COOKIE_NAME = 'nf_ab'
+
 export default async (request: Request, context: Context) => {
   const url = new URL(request.url);
 
   switch (url.searchParams.get("action")) {
     case "set":
       context.cookies.set({
-        name: "action",
-        value: "hello",
+        name: COOKIE_NAME,
+        value: "test",
       });
 
       return new Response('Cookie value has been set. Reload this page without the "action" parameter to see it.');
 
     case "clear":
-      context.cookies.delete("action");
+      context.cookies.delete(COOKIE_NAME);
 
       return new Response(
         'Cookie value has been cleared. Reload this page without the "action" parameter to see the new state.',
       );
   }
 
-  const value = context.cookies.get("action");
+  const value = context.cookies.get(COOKIE_NAME);
   const message = value
     ? `Cookie value is "${value}". You can clear it by using "?action=clear".`
     : 'Cookie has not been set. You can do so by adding "?action=set" to the URL.';
